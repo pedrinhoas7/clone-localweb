f88e4b4555507fe092844dc17247df433ac7430c
Pedro Henrique de Moraes Batista <pedro.moraes@vsoft.com.br>
2023-04-15 15:07:19 -0300

    #feature/consumer-api

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,25 +1,56 @@
-<script setup lang="ts">
-
-</script>
-
-<template>
+<template #default>
   <div class="container">
-    <div >
+    <div>
       <img alt="Vue logo" class="logo" src="@/assets/logo.png" width="200" height="40" />
       <div class="card">
         <h2>Entre na sua conta</h2>
         <h3>Para acessar sua conta informe seu e-mail e senha</h3>
         <label>E-mail</label>
-        <input class="input" placeholder="Seu e-mail" />
+        <input class="input" placeholder="Seu e-mail" type="username" v-model="username" required/>
         <label>Senha</label>
-        <input class="input" placeholder="Sua senha" />
+        <input class="input" placeholder="Sua senha"  type="password" v-model="password" required/>
         <a>Esqueci minha senha</a>
-        <button>FAZER LOGIN</button>
+        <button @click="submit">FAZER LOGIN</button>
       </div>
       <p class="link">Ainda não tem conta?<a>Cadastre-se</a></p>
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import api from '@/plugin/api';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    async submit() {
+      try {
+        // fazer a chamada de autenticação aqui
+        // por exemplo, usando uma API
+        const response = await api.post('/auth/login', {
+          username: this.username,
+          password: this.password,
+        });
+
+        // se a autenticação foi bem-sucedida, redirecionar para a página inicial
+        if (response.status === 200) {
+          this.$router.push('/');
+        }
+      } catch (error) {
+        console.log(error)
+        // se ocorreu um erro na autenticação, exibir uma mensagem de erro
+        alert('username ou senha incorretos');
+      }
+    },
+  },
+});
+</script>
 
 <style>
 .container {
@@ -27,13 +58,15 @@
   display: inline-block;
   justify-content: center;
 }
-.card{
+
+.card {
   background-color: white;
   width: 500px;
   display: inline-block;
   border: 1px solid #ccc;
 }
-.input{
+
+.input {
   width: 100%;
   padding: 12px 20px;
   margin: 8px 0;
@@ -51,6 +84,4 @@ button {
   cursor: pointer;
   width: 100%;
 }
-
-
 </style>
