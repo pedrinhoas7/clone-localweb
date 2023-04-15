1e05709e6e0c25a37f7c87439f639ef71c22132c
Pedro Henrique de Moraes Batista <pedro.moraes@vsoft.com.br>
2023-04-15 18:49:57 -0300

    #feature/create-auth

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,18 @@
 import { createRouter, createWebHistory, type NavigationGuard } from 'vue-router'
+
 import LoginView from '../views/LoginView.vue';
 import WathStream from '@/views/WathStream.vue';
 import PlansView from '@/views/PlansView.vue';
 import vm from '@/views/MainViewModel';
-
-const adminNavigationGuard: NavigationGuard = (to, from, next) => vm.user != null ? next() : next({ name: 'login' })
+const requireAuth: NavigationGuard = (to, from, next) => {
+  console.log(vm.getUser())
+  if (vm.getUser() == null && to.name == 'watch') {
+    console.log(vm.getUser())
+    next({ name: 'login' });
+  }else{
+    next()
+  }
+};
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,7 +20,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'plans',
-      component: PlansView
+      component: PlansView,
     },
     {
       path: '/login',
@@ -22,7 +30,8 @@ const router = createRouter({
     {
       path: '/watch',
       name: 'watch',
-      component: WathStream
+      component: WathStream,
+      beforeEnter: requireAuth
     }
   ]
 })
